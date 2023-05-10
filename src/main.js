745e4c39e34b099b1bd4ae69917bdb7866dd8958
Zelo <46229265+MyNameIsZelo@users.noreply.github.com>
2023-05-10 21:12:23 +1000

    Update and rename App.js to main.js

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function Main() {
+  return (
+    <body>
+      <div className="header">
+        <button>Movies</button>
+        <button>Register</button>
+        <button>Login</button>
+        <style className="app">
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start editing to see some magic happen!</h2>
+        </style>
+      </div>
+    </body>
+  );
+}

8ed3395be51963db0d1a2fff86b0d632c7854b1a
Zelo <46229265+MyNameIsZelo@users.noreply.github.com>
2023-05-10 21:13:16 +1000

    Update movie.js

--- src/movie.js
+++ src/movie.js
@@ -1,1 +1,10 @@
+import "./styles.css";
 
+export default function Movie() {
+  return (
+    <div className="Movie">
+      <h1>Movies</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
